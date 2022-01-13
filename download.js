5a2c4c55c04c5fe60d8b31e7799adefe3b62e12d
Ricardo Carvalho <ricardo.carvalho@integer.pt>
2022-01-13 11:51:17 +0000

    Adding video format selector

--- download.js
+++ download.js
@@ -1,72 +1,82 @@
-(function($) {
-	$(document).ready(function() {
-		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+(function ($) {
+    $(document).ready(function () {
+        chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-			// Source: http://pixelscommander.com/en/javascript/javascript-file-download-ignore-content-type/
-			window.downloadFile = function (sUrl) {
+            // Source: http://pixelscommander.com/en/javascript/javascript-file-download-ignore-content-type/
+            window.downloadFile = function (sUrl) {
 
-				//iOS devices do not support downloading. We have to inform user about this.
-				if (/(iP)/g.test(navigator.userAgent)) {
-					//alert('Your device does not support files downloading. Please try again in desktop browser.');
-					window.open(sUrl, '_blank');
-					return false;
-				}
+                //iOS devices do not support downloading. We have to inform user about this.
+                if (/(iP)/g.test(navigator.userAgent)) {
+                    //alert('Your device does not support files downloading. Please try again in desktop browser.');
+                    window.open(sUrl, '_blank');
+                    return false;
+                }
 
-				//If in Chrome or Safari - download via virtual link click
-				if (window.downloadFile.isChrome || window.downloadFile.isSafari) {
-					//Creating new link node.
-					var link = document.createElement('a');
-					link.href = sUrl;
-					link.setAttribute('target','_blank');
+                //If in Chrome or Safari - download via virtual link click
+                if (window.downloadFile.isChrome || window.downloadFile.isSafari) {
+                    //Creating new link node.
+                    var link = document.createElement('a');
+                    link.href = sUrl;
+                    link.setAttribute('target', '_blank');
 
-					if (link.download !== undefined) {
-						//Set HTML5 download attribute. This will prevent file from opening if supported.
-						var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
-						link.download = fileName;
-					}
+                    if (link.download !== undefined) {
+                        //Set HTML5 download attribute. This will prevent file from opening if supported.
+                        var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+                        link.download = fileName;
+                    }
 
-					//Dispatching click event.
-					if (document.createEvent) {
-						var e = document.createEvent('MouseEvents');
-						e.initEvent('click', true, true);
-						link.dispatchEvent(e);
-						return true;
-					}
-				}
+                    //Dispatching click event.
+                    if (document.createEvent) {
+                        var e = document.createEvent('MouseEvents');
+                        e.initEvent('click', true, true);
+                        link.dispatchEvent(e);
+                        return true;
+                    }
+                }
 
-				// Force file download (whether supported by server).
-				if (sUrl.indexOf('?') === -1) {
-					sUrl += '?download';
-				}
+                // Force file download (whether supported by server).
+                if (sUrl.indexOf('?') === -1) {
+                    sUrl += '?download';
+                }
 
-				window.open(sUrl, '_blank');
-				return true;
-			};
+                window.open(sUrl, '_blank');
+                return true;
+            };
 
-			window.downloadFile.isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-			window.downloadFile.isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+            window.downloadFile.isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+            window.downloadFile.isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
 
-			if( request.getDataFrom9Gag ){
-				sendResponse({postPitle: $('section#individual-post header h1').text()});
-			}
+            if (request.getDataFrom9Gag) {
+                var $videoContainerSources = $('#container .post-container video source'),
+                    videoContainerSourcesArray = [];
 
-			if( request.downloadFrom9Gag ){
-				console.log('downloadFrom9Gag');
-				if( $('#container .post-container video').length ){
-					var sourceMP4 = $('#container .post-container video source:eq(1)');
-					// window.open( sourceMP4.attr('src'), '_blank' );
-					downloadFile( sourceMP4.attr('src') );
-				} else if( $('#container .post-container picture').length ){
-					var sourceJPG = $('#container .post-container picture img');
-					// window.open( sourceJPG.attr('src'), '_blank' );
-					downloadFile( sourceJPG.attr('src') );
-				}
+                for (var i = 0; i < $videoContainerSources.length; i++) {
+                    videoContainerSourcesArray.push($videoContainerSources.eq(i).attr('src'))
+                }
 
-				$('body').addClass('9gag-downloader');
-				return true;
-			}
+                sendResponse({
+                    postPitle: $('section#individual-post header h1').text(),
+                    postVideos: videoContainerSourcesArray,
+                });
+            }
 
-		})
+            if (request.downloadFrom9Gag) {
+                // console.log('downloadFrom9Gag', request);
+                var $videoContainer = $('#container .post-container video'),
+                    $pictureContainer = $('#container .post-container picture');
 
-	});
+                if ($videoContainer.length) {
+                    downloadFile(request.selectedSource);
+                } else if ($pictureContainer.length) {
+                    var sourceJPG = $pictureContainer.find('img');
+                    downloadFile(sourceJPG.attr('src'));
+                }
+
+                $('body').addClass('9gag-downloader');
+                return true;
+            }
+
+        })
+
+    });
 })(jQuery);
--- popup.js
+++ popup.js
@@ -1,12 +1,40 @@
-$('#9gag-download-btn').click(function() {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage( tabs[0].id, { downloadFrom9Gag: true } );
-	});
+$('#9gag-download-btn').click(function () {
+    var selectedSource = $('#video-format-selector').val();
+
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {downloadFrom9Gag: true, selectedSource: selectedSource});
+    });
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, { getDataFrom9Gag: true }, function(response) {
-		$('#post-title').text(response.postPitle);
-		console.log(response.postPitle);
-	});
+chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {getDataFrom9Gag: true}, function (response) {
+
+        console.log(response);
+
+        var $postTitle = $('#post-title'),
+            $formatSelector = $('#video-format-selector');
+
+        function getUrlExtension(url) {
+            return url.split(/[#?]/)[0].split('.').pop().trim();
+        }
+
+        if (response.postVideos.length) {
+            var sources = $.unique(response.postVideos);
+
+            $formatSelector.html('');
+            for (var i = 0; i < sources.length; i++) {
+                $formatSelector.append($('<option>', {
+                    value: sources[i],
+                    text: getUrlExtension(sources[i])
+                }));
+            }
+        } else {
+            $('#video-format-wrapper').hide();
+        }
+
+        $postTitle.text('');
+
+        if (response.postPitle)
+            $postTitle.text(response.postPitle);
+    });
 });
